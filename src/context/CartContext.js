526a129053e50fffc0043b89fc40bbaac278d06f
Ryan Azur <kodegowd64@gmail.com>
2023-06-01 10:33:48 +0800

    01CartContext

---
+++ src/context/CartContext.js
@@ -0,0 +1,22 @@
+import { useContext } from 'react';
+import { createContext } from 'react';
+
+const initialState = {
+  cartList: [],
+  total: 0,
+};
+
+const CartContext = createContext(initialState);
+
+export const CartProvider = ({ children }) => {
+  const value = {
+    total: 100,
+  };
+
+  return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
+};
+
+export const useCart = () => {
+  const context = useContext(CartContext);
+  return context;
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
+import { CartProvider } from './context/CartContext';
 import './index.css';
 import App from './App';
 
@@ -8,7 +9,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <CartProvider>
+        <App />
+      </CartProvider>
     </Router>
   </React.StrictMode>
 );
