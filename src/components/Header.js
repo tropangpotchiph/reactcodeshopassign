83f8f9dd9c7ec8e67053c9c203ccd74826e7f6ae
Ryan Azur <kodegowd64@gmail.com>
2023-06-01 10:10:14 +0800

    Starter File

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,24 @@
-import { Link, NavLink } from "react-router-dom";
-import Logo from "../assets/logo.png";
-import "./Header.css";
+import { Link, NavLink } from 'react-router-dom';
+import Logo from '../assets/logo.png';
+import './Header.css';
 
 export const Header = () => {
   return (
     <header>
-      <Link
-        to="/"
-        className="logo"
-      >
-        <img
-          src={Logo}
-          alt="Kodego Logo"
-        />
+      <Link to="/" className="logo">
+        <img src={Logo} alt="Kodego Logo" />
         <span>Kodeshop</span>
       </Link>
       <nav className="navigation">
-        <NavLink
-          to="/"
-          className="link"
-          end
-        >
+        <NavLink to="/" className="link" end>
           Home
         </NavLink>
-        <NavLink
-          to="/cart"
-          className="link"
-        >
+        <NavLink to="/cart" className="link">
           Cart
         </NavLink>
       </nav>
-      <Link
-        to="/cart"
-        className="items"
-      >
-        <span>Cart: 2</span>
+      <Link to="/cart" className="items">
+        <span>Cart: 5</span>
       </Link>
     </header>
   );
